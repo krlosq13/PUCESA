52345ba89b0c1486375a157729ffbcac843319a5
Edgar Apaza <edgarapazac@gmail.com>
2016-07-06 16:18:36 -0500

    Agregando funcionalidad a la clase Curso para Matriculas

--- Models/CursoClass.php
+++ Models/CursoClass.php
@@ -28,23 +28,55 @@
 			return $datos;
 		}
 
-		public function mostrarCurso(){
-			$sql = "SELECT cursos, idmodulo FROM educacion.curso where idcurso = 1";
+		public function mostrarCurso($idcurso){
+			$sql = "SELECT cursos, idmodulo FROM curso where idcurso = $idcurso";
 			$datos = $this->mysqli->query($sql) or die("Error: ".mysql_error());
-			return $datos;
+			$fila = $datos->fetch_assoc();
+			return $fila;
 		}
 
+		public function mostrarDocente($idDocente){
+			$sql = "SELECT CONCAT(nombres,' ', apellidos) as docente FROM docentes WHERE idDocentes = $idDocente";
+			$datos = $this->mysqli->query($sql) or die("Error: ".mysql_error());
+			$fila = $datos->fetch_assoc();
+			return $fila;
+		}
+
+		public function mostrarFrecuencia($idfrecuencia){
+			$sql = "SELECT frecuencia FROM frecuencia WHERE idfrecuencia = $idfrecuencia";
+			$datos = $this->mysqli->query($sql) or die("Error: ".mysql_error());
+			$fila = $datos->fetch_assoc();
+			return $fila;
+		}
+
+		public function mostrarHorario($idHorario){
+			$sql = "SELECT horas FROM horario WHERE idhorario = $idHorario";
+			$datos = $this->mysqli->query($sql) or die("Error: ".mysql_error());
+			$fila = $datos->fetch_assoc();
+			return $fila;
+		}
 	}
 
 	$curso = new Curso();
 	$data = $curso->CursosActivos();
+
 	while ($row = $data->fetch_assoc()) {
 		echo $row['idcargocurso']."<br>";
-		echo $row['idCurso']."<br>";
-		echo $row['idDocente']."<br>";
-		echo $row['idfrecuencua']."<br>";
-		echo $row['idHorario']."<br>";
+
+		$cur = $curso->mostrarCurso($row['idCurso']);
+		echo $cur['cursos'];
+
+		$doc = $curso->mostrarDocente($row['idDocente']);
+		echo $doc['docente'];
+
+		$frec = $curso->mostrarFrecuencia($row['idfrecuencia']);
+		echo $frec['frecuencia'];
+
+		$hor = $curso->mostrarHorario($row['idHorario']);
+		echo $hor['horas'];
 		echo $row['fechaInicio']."<br>";
 		echo $row['fechaFinal']."<br>";
 	}
+
+
 ?>
